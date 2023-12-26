1115c4095eb237eb1d3f88327c7b3edad053d8ac
Yassine Nassibi <ynassibi@e1r3p8.1337.ma>
2023-12-26 14:38:47 +0100

    server at 30

--- Consumer.c
+++ Consumer.c
@@ -6,7 +6,7 @@
 /*   By: ynassibi <ynassibi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/26 11:09:00 by ynassibi          #+#    #+#             */
-/*   Updated: 2023/12/26 12:07:45 by ynassibi         ###   ########.fr       */
+/*   Updated: 2023/12/26 14:37:34 by ynassibi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void    pid_handler(siginfo_t *sig_infos, int *client_pid, int *actual_pid)
     *actual_pid = sig_infos->si_pid;
 }
 
-void    bit_handler(int sig_v, int *index, unsigned char *carac)
+void    bit_handler(int sig_v, int *index,char *carac)
 {
     if (sig_v == SIGUSR2)
         *carac |= (0x01 << *index);
@@ -27,7 +27,7 @@ void    bit_handler(int sig_v, int *index, unsigned char *carac)
         *carac |= (0x0 << *index);
 }
 
-void    caracter_handler(int c_pid, unsigned char *o, int *s_mssg, int *i)
+void    caracter_handler(int c_pid,char *o, int *s_mssg, int *i)
 {
     if (*o == '\0')
     {
@@ -39,52 +39,61 @@ void    caracter_handler(int c_pid, unsigned char *o, int *s_mssg, int *i)
     *i = 0x0;
 }
 
-void    recieve_mssg(int signal_value, siginfo_t *sig_infos, void *nothing)
+void    recieve_mssg(int sig, siginfo_t *sig_infos, void *nothing)
 {
-    static int                signal_message;
-    static int                client_pid;
-    static int                actual_pid;
-    static int                index;
-    static unsigned char    received_carac;
+    static int                msg;
+    static int                id_clt;
+    static char    received_carac;
+    static int                id_cur;
+    static int                i;
 
     (void)nothing;
-    pid_handler(sig_infos, &client_pid, &actual_pid);
-    if (client_pid != actual_pid)
+    pid_handler(sig_infos, &id_clt, &id_cur);
+    if (id_clt != id_cur)
     {
-        client_pid = sig_infos->si_pid;
-        received_carac = 0x0;
-        index = 0x0;
-        signal_message = 0x0;
+        id_clt = sig_infos->si_pid;
+        received_carac = 0;
+        i = 0;
+        msg = 0;
     }
-    bit_handler(signal_value, &index, &received_carac);
-    index++;
-    signal_message++;
-    if (index == 0x8)
+    bit_handler(sig, &i, &received_carac);
+    i++;
+    msg++;
+    if (i == 8)
     {
-        write (1, &received_carac, 1);
-        caracter_handler(client_pid, &received_carac, &signal_message, &index);
+        ft_write (received_carac);
+        caracter_handler(id_clt, &received_carac, &msg, &i);
     }
     usleep(100);
-    kill(client_pid, SIGUSR2);
+    kill(id_clt, SIGUSR2);
 }
 
-int    main(void)
+void ft_sigactions (int usr_1, int usr_2 , struct sigaction *sig)
 {
-    int                    pid;
-    struct sigaction    signals_handler;
+	sigaction(SIGUSR1, sig, 0x0);
+		sigaction(SIGUSR2, sig, 0x0);
+}
+void ft_sigactions_set (struct sigaction *obj)
+{
+	obj->sa_flags = 0x0040;
+	obj->sa_sigaction = recieve_mssg;
+	 sigemptyset(&obj->sa_mask);
+}
 
-    pid = getpid();
-    sigemptyset(&signals_handler.sa_mask);
-    signals_handler.sa_flags = SA_SIGINFO;
-    signals_handler.sa_sigaction = recieve_mssg;
-    //ft_printf("My Server's PID : %d\n", pid);
-    ft_putnub(pid);
-    //ft_printf("\033[90mWaiting for a message...\033[0m\n");
-    while (1337)
-    {
-        sigaction(SIGUSR1, &signals_handler, NULL);
-        sigaction(SIGUSR2, &signals_handler, NULL);
-        pause();
-    }
-    return (0);
+int	main(void)
+{
+	struct sigaction	sig_obj;
+	int		id;
+	char life ;
+
+	 life = 'k';
+	id = getpid();
+	ft_sigactions_set (&sig_obj);
+	//ft_printf("My Server's PID : %d\n", pid);
+	ft_putnub(id);
+	//ft_printf("\033[90mWaiting for a message...\033[0m\n");
+	 ft_sigactions(SIGUSR1,SIGUSR2,&sig_obj);
+	while (life)
+		pause();
+	return (0);
 }
